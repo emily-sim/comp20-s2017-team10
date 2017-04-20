e4e9d204c27d6a37e293326a01569e0a9bf3ccff
Emily <emily.kiwan.sim@gmail.com>
2017-04-20 15:21:27 -0400

    change timer

--- front-end/game.js
+++ front-end/game.js
@@ -4,32 +4,31 @@ var demos = [];
 var answers = [];
 var correct_answer;
 var score = 0;
+var counter = 0;
 
-    function spotifyRequest(url) {
+function spotifyRequest(url) {
+  var request = new XMLHttpRequest();
+  request.open("GET", url, true);
+  request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  request.send();
+  request.onreadystatechange = function() {
+    if (request.readyState === 4 && request.status === 200) {
+      var data = JSON.parse(request.responseText);  
+      storeTrackData(data);                
+    } // else -- handle errors
+  }
+}
 
-        var request = new XMLHttpRequest();
-        request.open("GET", url, true);
-        request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        request.send();
-        request.onreadystatechange = function() {
-            if (request.readyState === 4 && request.status === 200) {
-                var data = JSON.parse(request.responseText);  
-                storeTrackData(data);                
-            } // else -- handle errors
-        }
-
-    }
-
-    function storeTrackData(data) {
-      for (var i = 0; i < 4; i++) {
-        tracks[i] = data.tracks.items[i].name;
-        // img 300px x 300px on API
-        albumcovers[i] = data.tracks.items[i].album.images[1].url;
-        demos[i] = data.tracks.items[i].preview_url;
-        console.log("from spotify request: " + tracks[i] + " " + albumcovers[i] + " " + demos[i]);
-      }  
-      updateChoices();
-    }
+function storeTrackData(data) {
+  for (var i = 0; i < 4; i++) {
+    tracks[i] = data.tracks.items[i].name;
+    // img 300px x 300px on API
+    albumcovers[i] = data.tracks.items[i].album.images[1].url;
+    demos[i] = data.tracks.items[i].preview_url;
+    console.log("from spotify request: " + tracks[i] + " " + albumcovers[i] + " " + demos[i]);
+  }  
+  updateChoices();
+}
 
     /*  client side:
      * 1) while loop for 10 songs for one round 
@@ -42,45 +41,44 @@ var score = 0;
      * 1) get request user info via \players
      * 2) send new high score back using post request \submit
     */
-    function runGame() {
-      var user_id = 135   // hard coded for now
-      var highscore = getUserInfo(user_id);
-      var offset = getRandomArbitrary(0, 50);
-      var genre = getQuery();
+function runGame() {
+  var user_id = 135   // hard coded for now
+  var highscore = getUserInfo(user_id);
+  var offset = getRandomArbitrary(0, 50);
+  var genre = getQuery();
 
 
-      //for (var count = 0; count < 10; count++) {
-        //play song  http://html.com/attributes/audio-src/
-        loadPlaylist(genre, offset);
-        var random = getRandomArbitrary(0, 4);  // min inclusive, max exclusive
-        correct_answer = random;
-        var demo_track = demos[random];
-        document.getElementById("demo").src=demo_track;
+  
+    //play song  http://html.com/attributes/audio-src/
+    loadPlaylist(genre, offset);
+    correct_answer = getRandomArbitrary(0, 4);  // min inclusive, max exclusive
+    var demo_track = demos[random];
+    document.getElementById("demo").src=demo_track;
+    startTimer();
+    offset = offset + 4;
+  }
+}
 
-        offset = offset + 4;
-      //}
-    }
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
 
-    function getRandomArbitrary(min, max) {
-     return Math.random() * (max - min) + min;
-    }
+function updateChoices() {
+  var img_src = albumcovers[0]
+  document.getElementById("button-album-cover-0").src=img_src;
+  img_src = albumcovers[1]
+  document.getElementById("button-album-cover-1").src=img_src;
+  img_src = albumcovers[2]
+  document.getElementById("button-album-cover-2").src=img_src;
+  img_src = albumcovers[3]
+  document.getElementById("button-album-cover-3").src=img_src;
 
-    function updateChoices() {
-      var img_src = albumcovers[0]
-      document.getElementById("button-album-cover-0").src=img_src;
-      img_src = albumcovers[1]
-      document.getElementById("button-album-cover-1").src=img_src;
-      img_src = albumcovers[2]
-      document.getElementById("button-album-cover-2").src=img_src;
-      img_src = albumcovers[3]
-      document.getElementById("button-album-cover-3").src=img_src;
-
-      /* not showing up currently */
-      document.getElementById("button-album-cover-0").innerHTML = tracks[0];
-      document.getElementById("button-album-cover-1").innerHTML = tracks[1];
-      document.getElementById("button-album-cover-2").innerHTML = tracks[2];
-      document.getElementById("button-album-cover-3").innerHTML = tracks[3];
-    }
+  /* not showing up currently */
+  document.getElementById("button-album-cover-0").innerHTML = tracks[0];
+  document.getElementById("button-album-cover-1").innerHTML = tracks[1];
+  document.getElementById("button-album-cover-2").innerHTML = tracks[2];
+  document.getElementById("button-album-cover-3").innerHTML = tracks[3];
+}
 
     function getUserInfo(id) {
       var request = new XMLHttpRequest();
@@ -108,7 +106,7 @@ var score = 0;
       spotifyRequest("https://api.spotify.com/v1/search?q=genre%3A" + genre + "&type=track&market=US&limit=4&offset=" + offset);
     }
 
-    function move() {
+    function startTimer() {
         var elem = document.getElementById("bar");
         var time = document.getElementById("time");
         var width = 0;
@@ -152,6 +150,6 @@ var score = 0;
           score += 1;
 
         } 
-        
+
         scoreDisplay.innerHTML = "Score: " + score;
     };
