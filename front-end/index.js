81d34c3d6ce23c904bd432576c8ec4341f64be55
Jose <jlemus1234@gmail.com>
2017-04-23 02:09:47 -0400

    Side bar is loading the profile picture and name. FB login and logout buttons are shown and hidden correctly. Simplified index file.

--- front-end/index.js
+++ front-end/index.js
@@ -1,117 +1,102 @@
-  // This is called with the results from from FB.getLoginStatus().
-  function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
-    if (response.status === 'connected') {
-      // Logged into your app and Facebook.
-//      testAPI();
+// This is called with the results from from FB.getLoginStatus().
+function statusChangeCallback(response) {
+  var loginButton = document.getElementById("loginButton");
+  var logoutButton = document.getElementById("logoutButton");
+  var profilePic = document.getElementById("profile");
+  var nameLabel = document.getElementById("name");
 
 
+  if (response.status === 'connected') {
+    // Logged into your app and Facebook.
+    logoutButton.style.visibility = "visible";
+    loginButton.style.visibility = "hidden";
 
-// TEST DATA
-// Uploading to the database
-// currently sending id and last name
-      console.log("testing");
-        FB.api (
-        '/me', {fields: "last_name"},
-        function (response) {
-          console.log("inside function");
-          console.log(response);
-          if (response && !response.error) {
-  //          var oReq = new XMLHttpRequest();
-  //          var url = "https://musicguess.herokuapp.com";
-         //   var params = "user-id=" + response.id;
-            console.log("RESPONSE THING BEING CHECKED");
-            console.log(response);
-
-            var oReq = new XMLHttpRequest();
-            var url = "https://musicguessing.herokuapp.com/submit";
-//            var url = "http://localhost:5000/submit"
-            var params = "userid=" + response.id + "&username=" + response.last_name;
-            oReq.open("POST", url, true);
-            oReq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            oReq.send(params);
-            console.log(response.id);
-            console.log("Should have just sent the request");
-            oReq.onreadystatechange = function(){
-              console.log("Changing state");
-              if(oReq.readyState === 4 && oReq.status === 200){
-                var resp = oReq.responseText;
-                resp = (JSON.parse(resp));
-                console.log(resp);
-              }
+      FB.api (
+      '/me', {fields: "first_name, last_name"},
+      function (response) {
+        console.log("Inside FB API function");
+        console.log(response);
+        if (response && !response.error) {
+          var name = response.first_name + " " + response.last_name;
+          nameLabel.innerHTML = name;
+          var oReq = new XMLHttpRequest();
+          var url = "https://musicguessing.herokuapp.com/user";
+          var params = "userid=" + response.id;
+          oReq.open("POST",url,true);
+          oReq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+          oReq.send(params);
+          oReq.onreadystatechange = function(){
+            if(oReq.readyState === 4 && oReq.status === 200){
+              var resp = oReq.responseText;
+              resp = (JSON.parse(resp));
             }
-
           }
+
         }
-        );
+      }
+      );
 
-    } else {
-      // The person is not logged into your app or we are unable to tell.
-      //document.getElementById('status').innerHTML = 'Please log ' +
-      //  'into this app.';
-      console.log("Not logged in");
-    }
+      /* make the API call */
+      FB.api(
+          "/me/picture?type=large",
+          function (response) {
+            if (response && !response.error) {
+                profilePic.src = response.data.url;
+            }
+          }
+      );
 
-
+  } else {
+    loginButton.style.visibility = "visible";
+    logoutButton.style.visibility = "hidden";
+    nameLabel.innerHTML = "Guest";
+    profilePic.src = "images/pop.jpg";
+    console.log("Not logged in");
   }
+}
 
-  // This function is called when someone finishes with the Login
-  // Button.  See the onlogin handler attached to it in the sample
-  // code below.
-  function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
-    });
-  }
 
-  window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '839801796183910',
-    cookie     : true,  // enable cookies to allow the server to access
-                        // the session
-    xfbml      : true,  // parse social plugins on this page
-    version    : 'v2.8' // use graph api version 2.8
+function fblogin(){
+  FB.login(function(response){
+    statusChangeCallback(response);
   });
+}
 
-  // Now that we've initialized the JavaScript SDK, we call
-  // FB.getLoginStatus().  This function gets the state of the
-  // person visiting this page and can return one of three states to
-  // the callback you provide.  They can be:
-  //
-  // 1. Logged into your app ('connected')
-  // 2. Logged into Facebook, but not your app ('not_authorized')
-  // 3. Not logged into Facebook and can't tell if they are logged into
-  //    your app or not.
-  //
-  // These three cases are handled in the callback function.
-
+function fblogout(){
+FB.logout(function(response){
+  statusChangeCallback(response);
+  });
+}
+// This function is called when someone finishes with the Login
+// Button.
+function checkLoginState() {
   FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
   });
+}
 
-  };
+window.fbAsyncInit = function() {
+FB.init({
+  appId      : '839801796183910',
+  cookie     : true,  // enable cookies to allow the server to access
+                      // the session
+  xfbml      : true,  // parse social plugins on this page
+  version    : 'v2.8' // use graph api version 2.8
+});
 
-  // Load the SDK asynchronously
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
+// Gets the state of the person visiting this page and can return one
+// of three states to the callback you provide.
+FB.getLoginStatus(function(response) {
+  statusChangeCallback(response);
+});
 
-  // Here we run a very simple test of the Graph API after login is
-  // successful.  See statusChangeCallback() for when this call is made.
-    function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name);
-//      document.getElementById('status').innerHTML =
-//        'Thanks for logging in, ' + response.name + '!';
-    });
-  }
+};
+
+// Load the SDK asynchronously
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
